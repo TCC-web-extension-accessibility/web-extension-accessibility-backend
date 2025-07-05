d6f45dc0d9dbe8898de59bcfa195460a35f11958
VitorPirolo19 <vpirolo10@gmail.com>
2025-07-05 01:25:13 -0300

    feat: create SQLalchemy User model

---
+++ app/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from core.database import Base
+
+class User_model(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String)
+    hashed_password = Column(String, nullable=False)
+    disabled = Column(Boolean, default=False)
