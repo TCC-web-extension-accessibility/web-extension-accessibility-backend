2a2ed2ec0cb607f63fb276af70bdb1bbac45852d
VitorPirolo19 <vpirolo10@gmail.com>
2025-10-06 00:03:29 -0300

    fix: imports in tests

--- app/services/image_description.py
+++ app/services/image_description.py
@@ -19,7 +19,8 @@ def analyze_image(image_bytes: bytes) -> str:
     except ClientAuthenticationError as authError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access denied due to invalid subscription key or wrong API endpoint.")
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"caption": "error analyzing image"})
+        print(e)
+        return {"caption": "error analyzing image"}
         
     if result and result.caption is not None:
         return {"caption": result.caption.text}
